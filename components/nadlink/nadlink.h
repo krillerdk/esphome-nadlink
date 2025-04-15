6f1dbc27b8b7d382b93b34b8daaffe497e1e2773
Kristian Sørensen <ksorensen@septima.dk>
2025-04-15 18:45:20 +0200

    Fix missing ;

--- components/nadlink/nadlink.h
+++ components/nadlink/nadlink.h
@@ -34,7 +34,7 @@ class NadLink : public Component {
   void standby_toggle();
   void send_command(uint8_t command);
 
-  GPIOPin *nadlink_signal_pin
+  GPIOPin *nadlink_signal_pin;
   void set_nadlink_signal_pin(GPIOPin *pin ) { nadlink_signal_pin = pin; }
   void set_parent(button::Button *btn) { this->parent_button_ = btn; }
 
