62ceeaa83ca5753155276346a879a501b3414b64
Kristian Sørensen <ksorensen@septima.dk>
2025-04-16 17:14:54 +0200

    Fiddle with constness

--- components/nadlink/nadlink.h
+++ components/nadlink/nadlink.h
@@ -35,7 +35,7 @@ class NADLink : public Component {
   void toggle_standby();
   
  protected:
-  static constexpr char* TAG = "nadlink";
+  const char* const TAG = "nadlink";
   uint8_t nadlink_pin_{13};
 
   bool power_is_on_{false};
