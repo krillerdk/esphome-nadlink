f6d394d846f9d799c2aea99bfa7649afef2961e6
Kristian Sørensen <kriller@kriller.dk>
2025-10-25 15:11:49 +0200

    No async for simple adds

--- components/nadlink/__init__.py
+++ components/nadlink/__init__.py
@@ -146,13 +146,13 @@ async def to_code(config):
 
     # Set the NAD protocol address if specified
     if CONF_ADDRESS1 and CONF_ADDRESS2 in config:
-        await cg.add(var.set_nad_address(CONF_ADDRESS1, CONF_ADDRESS2))
+        cg.add(var.set_nad_address(CONF_ADDRESS1, CONF_ADDRESS2))
 
     # Volume level defaults
     if CONF_MAX_VOLUME in config:
-        await cg.add(var.set_max_assumed_volume(CONF_MAX_VOLUME))
+        cg.add(var.set_max_assumed_volume(CONF_MAX_VOLUME))
     if CONF_DEFAULT_VOLUME in config:
-        await cg.add(var.set_default_volume(CONF_DEFAULT_VOLUME))
+        cg.add(var.set_default_volume(CONF_DEFAULT_VOLUME))
         
     # Volume buttons
     if config[CONF_VOLUME_BUTTONS]:
